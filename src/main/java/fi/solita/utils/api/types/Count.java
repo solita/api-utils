73c602da114a01fab675c7988a81d43a61f7194c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2020-03-24 21:15:07 +0200

    allow 250 for paging

--- src/main/java/fi/solita/utils/api/types/Count.java
+++ src/main/java/fi/solita/utils/api/types/Count.java
@@ -9,7 +9,7 @@ import fi.solita.utils.api.Documentation;
 @Documentation(name_en = "Count", description = "Tulosjoukon maksimikoko. Oletuksena rajoittamaton", description_en = "Result set size. Unlimited by default")
 public final class Count {
     public static final Count DEFAULT = new Count(Integer.MAX_VALUE);
-    public static final List<Integer> validValues = newList(1,10,50,100,500,1000,4000);
+    public static final List<Integer> validValues = newList(1,10,50,100,250,500,1000,4000);
     
     public final int value;
     
