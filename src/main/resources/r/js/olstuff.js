b91878a914016151f86baef2c87938564e0f47e5
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2021-11-09 14:57:36 +0200

    updated openlayers and added rotation

--- src/main/resources/r/js/olstuff.js
+++ src/main/resources/r/js/olstuff.js
@@ -1,19 +1,15 @@
 var olstuff = function(constants, util) {
     proj4.defs("EPSG:3067", "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs");
+    ol.proj.proj4.register(proj4);
     
     var instant = new URLSearchParams(window.location.search).get('time') || util.now();
     
     var ret = {
         instant: instant,
         
-        projection: new ol.proj.Projection({
-            code: 'EPSG:3067',
-            extent: constants.world
-        }),
+        projection: ol.proj.get('EPSG:3067'),
         
-        geojsonProjection: new ol.proj.Projection({
-            code: 'EPSG:4326'
-        }),
+        geojsonProjection: ol.proj.get('EPSG:4326'),
         
         tileGrid: new ol.tilegrid.TileGrid({
             resolutions: constants.resolutions,
@@ -574,13 +570,16 @@ var olstuff = function(constants, util) {
                     new ol.control.Attribution({collapsible: false}),
                     new ol.control.Zoom(),
                     new ol.control.ZoomSlider(),
+                    new ol.control.Rotate(),
+                    new RotateLeftControl(),
+                    new RotateRightControl(),
                     new ol.control.ScaleLine(),
                     new ol.control.MousePosition({
                         coordinateFormat: function(c) {
                             return Math.round(c[0]) + "," + Math.round(c[1]);
                         }
                     }),
-                    new ol.control.LayerSwitcher()
+                    new ol.control.LayerSwitcher({ groupSelectStyle: 'children' })
                 ]
             });
         }
@@ -589,3 +588,55 @@ var olstuff = function(constants, util) {
     ol.proj.addProjection(ret.projection);
     return ret;
 };
+
+class RotateLeftControl extends ol.control.Control {
+    constructor(opt_options) {
+      const options = opt_options || {};
+      const button = document.createElement('button');
+      button.innerHTML = '⟲';
+  
+      const element = document.createElement('div');
+      element.className = 'rotate-left ol-unselectable ol-control';
+      element.setAttribute("title", "Vastapäivään 45°. Alt+shift+drag pyörittää vapaasti.");
+      element.appendChild(button);
+  
+      super({
+        element: element,
+        target: options.target,
+      });
+  
+      button.addEventListener('click', () => {
+        let view = this.getMap().getView();
+        view.animate({
+            duration: 250,
+            rotation: view.getRotation() - Math.PI / 4,
+        });
+      }, false);
+    }
+}
+
+class RotateRightControl extends ol.control.Control {
+    constructor(opt_options) {
+      const options = opt_options || {};
+      const button = document.createElement('button');
+      button.innerHTML = '⟳';
+  
+      const element = document.createElement('div');
+      element.className = 'rotate-right ol-unselectable ol-control';
+      element.setAttribute("title", "Myötäpäivään 45°. Alt+shift+drag pyörittää vapaasti.");
+      element.appendChild(button);
+  
+      super({
+        element: element,
+        target: options.target,
+      });
+  
+      button.addEventListener('click', () => {
+        let view = this.getMap().getView();
+        view.animate({
+            duration: 250,
+            rotation: view.getRotation() + Math.PI / 4,
+        });
+      }, false);
+    }
+}
