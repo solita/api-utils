5e7408e97923142cb24bed28a8a65fd85128eadd
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-01-24 11:24:22 +0200

    htmx: fix target-top for ctrl-clicks

--- src/main/resources/r/js/lib/htmx-ext-target-top.js
+++ src/main/resources/r/js/lib/htmx-ext-target-top.js
@@ -1,9 +1,16 @@
 htmx.defineExtension('target-top', {
     onEvent: function(name, evt) {
-        if (name === "htmx:beforeRequest") {
+        if (name === "htmx:beforeRequest" && !evt.detail.elt.hasAttribute('hx-target')) {
             evt.preventDefault();
             let anc = evt.detail.pathInfo.anchor;
-            window.location.assign(evt.detail.pathInfo.finalRequestPath + (anc ? '#' + anc : ''));
+            let triggeringEvent = evt.detail.requestConfig.triggeringEvent;
+            let targetPath = evt.detail.pathInfo.finalRequestPath + (anc ? '#' + anc : '');
+            if (triggeringEvent.ctrlKey || triggeringEvent.metaKey) {
+                window.open(targetPath, '_blank');
+            } else {
+                window.location.assign(targetPath);
+            }
+            return false;
         }
     }
 });
