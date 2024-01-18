65d32213f5bcbfe42cdb6cc549f162ca6d072765
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-18 12:04:44 +0200

    fix layers

--- src/main/resources/r/js/layers.js
+++ src/main/resources/r/js/layers.js
@@ -1,8 +1,11 @@
 var initLayers = function(map, olstuff, path) {
-    var ls = path.substring(path.lastIndexOf(';')+1).split(',');
-    map.getLayers().getArray().map(olstuff.actualLayers).reduce(function(a,b) {return a.concat(b)}).forEach(function(l) {
-        if (ls.includes(l.get('shortName'))) {
-            l.setVisible(true);
-        }
-    });
+    if (path.indexOf(';') >= 0) {
+        var ls = path.substring(path.lastIndexOf(';')+1).split(',');
+        map.getLayers().getArray().map(olstuff.actualLayers).reduce(function(a,b) {return a.concat(b)}).forEach(function(l) {
+            if (ls.includes(l.get('shortName'))) {
+                l.setVisible(true);
+            }
+        });
+        map.getControls().getArray().filter(function(x) { return x.showPanel; }).forEach(function(x) { x.showPanel(); x.hidePanel(); });
+    }
 };
