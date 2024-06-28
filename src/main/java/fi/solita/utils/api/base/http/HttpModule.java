8578beae9c865a62d56cf7f2fda5b45a5f0f8037
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-28 11:40:48 +0300

    allow http-deserialization of marked enums

--- src/main/java/fi/solita/utils/api/base/http/HttpModule.java
+++ src/main/java/fi/solita/utils/api/base/http/HttpModule.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.support.FormattingConversionService;
 
+import fi.solita.utils.api.base.StringToBeanDeserializableEnumConverter;
 import fi.solita.utils.api.base.StringToCollectionConverter;
 import fi.solita.utils.api.base.StringToOptionConverter;
 import fi.solita.utils.functional.Apply;
@@ -15,6 +16,7 @@ public final class HttpModule extends FormattingConversionService {
     public HttpModule(Map<Class<?>, Apply<String,?>> converters) {
         addConverter(new StringToCollectionConverter(this));
         addConverter(new StringToOptionConverter(this));
+        addConverter(new StringToBeanDeserializableEnumConverter());
         for (Map.Entry<Class<?>, Apply<String, ?>> e: converters.entrySet()) {
             addConverter(String.class, (Class<Object>)e.getKey(), new Converter<String,Object>() {
                 @Override
