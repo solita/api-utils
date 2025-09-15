56a96779ce17ebca03d03db233487c02fb8db5ef
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-09-15 11:27:28 +0300

    map: add some duration support

--- src/main/resources/r/js/time.js
+++ src/main/resources/r/js/time.js
@@ -3,13 +3,16 @@ var time = function(timeInput) {
     var params = new URLSearchParams(window.location.search);
     if (params.get('time')) {
         timeInput.value = params.get('time');
+        timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(new Date(timeInput.value), new Date())));
     }
     timeInput.onchange = function() {
         let params = new URLSearchParams(window.location.search);
         if (timeInput.value) {
             params.set('time', timeInput.value);
+            timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(new Date(timeInput.value), new Date())));
         } else {
             params.delete('time');
+            timeInput.removeAttribute('data-duration');
         }
         window.location.search = decodeURIComponent(params);
     }
