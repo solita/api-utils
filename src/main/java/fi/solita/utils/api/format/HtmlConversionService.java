fd9378867f6397d78f582056fe13fbe21491b388
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2026-03-05 19:15:02 +0200

    update tafs

--- src/main/java/fi/solita/utils/api/format/HtmlConversionService.java
+++ src/main/java/fi/solita/utils/api/format/HtmlConversionService.java
@@ -221,11 +221,11 @@ public abstract class HtmlConversionService {
                         .write(additionalHeadScript(), false)
                     ._script()
                     .script(type("text/javascript").src(contextPath + "/r/js/lib/Sortable.min.js"))._script()
-                    .script(type("text/javascript").src(contextPath + "/r/js/lib/tafs-1.5.0.min.js"))._script()
+                    .script(type("text/javascript").src(contextPath + "/r/js/lib/tafs-1.6.0.min.js"))._script()
                     .script(type("text/javascript"))
                         .write(scripts(), false)
                     ._script()
-                    .link(rel("stylesheet").href(contextPath + "/r/css/lib/tafs-1.5.0.css"));
+                    .link(rel("stylesheet").href(contextPath + "/r/css/lib/tafs-1.6.0.css"));
             }
         };
     }
