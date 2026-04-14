effd330b980d3e0f9b42e8a97f5fb795f8553c4c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2026-04-14 16:06:17 +0300

    fix map time input for intervals

--- src/main/resources/r/js/time.js
+++ src/main/resources/r/js/time.js
@@ -3,7 +3,9 @@ var time = function(timeInput, timeInputForce) {
     var params = new URLSearchParams(window.location.search);
     if (params.get('time')) {
         timeInput.value = params.get('time');
-        timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(new Date(timeInput.value), new Date())));
+        var start = timeInput.value.indexOf('/') < 0 ? new Date(timeInput.value) : new Date(timeInput.value.split('/')[0]);
+        var end = timeInput.value.indexOf('/') < 0 ? new Date() : new Date(timeInput.value.split('/')[1]);
+        timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(start, end)));
     }
     if (params.get('time-infra')) {
         timeInputForce.checked = true;
@@ -13,7 +15,9 @@ var time = function(timeInput, timeInputForce) {
             let params = new URLSearchParams(window.location.search);
             if (timeInput.value) {
                 params.set('time', timeInput.value);
-                timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(new Date(timeInput.value), new Date())));
+                var start = timeInput.value.indexOf('/') < 0 ? new Date(timeInput.value) : new Date(timeInput.value.split('/')[0]);
+                var end = timeInput.value.indexOf('/') < 0 ? new Date() : new Date(timeInput.value.split('/')[1]);
+                timeInput.setAttribute('data-duration', durationFns.durationFns.toString(durationFns.durationFns.between(start, end)));
             } else {
                 params.delete('time');
                 timeInput.removeAttribute('data-duration');
@@ -24,7 +28,7 @@ var time = function(timeInput, timeInputForce) {
     timeInputForce.onchange = function() {
         let params = new URLSearchParams(window.location.search);
         if (timeInputForce.checked && timeInput.value && timeInput.validity.valid) {
-            params.set('time-infra', timeInput.value);
+            params.set('time-infra', timeInput.value + (timeInput.value.indexOf('/') < 0 ? "/" + timeInput.value : ''));
         } else {
             params.delete('time-infra');
         }
